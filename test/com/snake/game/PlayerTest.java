ce68599a9750e667678bfafd4cb7af9bb1ef0609
Israel Kayode <israel.kayode@student.griffith.ie>
2026-03-27 21:39:40 +0000

    feat: implement Player with score tracking

--- test/com/snake/game/PlayerTest.java
+++ test/com/snake/game/PlayerTest.java
@@ -3,6 +3,9 @@ package com.snake.game;
 // By Israel Kayode
 // Student Number: 3167486
 
+import com.snake.model.Direction;
+import com.snake.model.PlayerType;
+import com.snake.model.Position;
 import com.snake.model.Snake;
 import org.junit.jupiter.api.Test;
 
@@ -11,35 +14,39 @@ import static org.junit.jupiter.api.Assertions.assertSame;
 
 public class PlayerTest {
 
+    private static Snake newTestSnake() {
+        return new Snake(new Position(5, 5), Direction.RIGHT, PlayerType.PLAYER1);
+    }
+
     @Test
     void testInitialScoreIsZero() {
-        Player player = new Player("P1", new Snake());
+        Player player = new Player("P1", newTestSnake());
         assertEquals(0, player.getScore());
     }
 
     @Test
     void testAddScoreIncreasesScore() {
-        Player player = new Player("P1", new Snake());
+        Player player = new Player("P1", newTestSnake());
         player.addScore(10);
         assertEquals(10, player.getScore());
     }
 
     @Test
     void testGetSnakeReturnsSnake() {
-        Snake snake = new Snake();
+        Snake snake = newTestSnake();
         Player player = new Player("P1", snake);
         assertSame(snake, player.getSnake());
     }
 
     @Test
     void testGetNameReturnsName() {
-        Player player = new Player("P1", new Snake());
+        Player player = new Player("P1", newTestSnake());
         assertEquals("P1", player.getName());
     }
 
     @Test
     void testResetSetsScoreToZero() {
-        Player player = new Player("P1", new Snake());
+        Player player = new Player("P1", newTestSnake());
         player.addScore(5);
         player.reset();
         assertEquals(0, player.getScore());
